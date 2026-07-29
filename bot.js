6864c75c3e22cf36482c43c0709fcb0dfe55582c
tungdps <tunsena2k14@gmail.com>
2026-07-30 01:06:13 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const req = require("request");
 const reqp = require("request-promise");
-const { EmbedBuilder } = require("discord.js"); // Cú pháp chuẩn discord.js v14
+const { EmbedBuilder } = require("discord.js");
 
 module.exports.gdpsstatus = async (client, M) => {
     let channel = await client.channels.fetch(M["channel"]["gdpsstatus"]);
@@ -12,7 +12,7 @@ module.exports.gdpsstatus = async (client, M) => {
                 let last = channel.messages;
                 let latest = await last.fetch();
                 let msg = latest.find(msg => msg.author.id == client.user.id);
-                let embed = new EmbedBuilder(); // Sửa MessageEmbed -> EmbedBuilder
+                let embed = new EmbedBuilder();
 
                 let dbStatus = "OK :green_circle:";
                 let httpdStatus = "OK :green_circle:";
@@ -31,17 +31,17 @@ module.exports.gdpsstatus = async (client, M) => {
                 embed.setDescription(db + "\n" + hosting + "\nThis will be refresh in 60 sec");
 
                 if (!msg) {
-                    embed.setFooter({ text: "times refreshed: " + 1 });
-                    return channel.send({ embeds: [embed] }); // Cú pháp send chuẩn v14
+                    embed.setFooter({ text: "times refreshed: 1" });
+                    return channel.send({ embeds: [embed] });
                 } else {
                     if (!msg.embeds[0] || !msg.embeds[0].footer) {
                         await msg.delete();
-                        embed.setFooter({ text: "times refreshed: " + 1 });
+                        embed.setFooter({ text: "times refreshed: 1" });
                         return channel.send({ embeds: [embed] });
                     }
                     let time = msg.embeds[0].footer.text.split(": ");
                     embed.setFooter({ text: time[0] + ": " + (parseInt(time[1]) + 1) });
-                    return msg.edit({ embeds: [embed] }); // Cú pháp edit chuẩn v14
+                    return msg.edit({ embeds: [embed] });
                 }
             });
         }, 60000);
@@ -75,7 +75,7 @@ module.exports.ratenotif = async (client, M) => {
                             info += "==============\n";
                             info += `**LevelID**: ${level["ID"]}\n`;
                             embed.setDescription(info);
-                            channel.send({ embeds: [embed] }); // Cú pháp send chuẩn v14
+                            channel.send({ embeds: [embed] });
                         });
                     });
                 }, 5000);
@@ -87,47 +87,3 @@ module.exports.ratenotif = async (client, M) => {
         }
     }, 10000);
 };
-							embed.setFooter(time[0] + ": " + (parseInt(time[1]) + 1));
-							return msg.edit(embed);
-							}
-					});
-				}, 60000);
-			} else {
-				return false;
-				}
-	}
-	
-module.exports.ratenotif = async(client, M) => {
-	let info;
-	let channel = await client.channels.fetch(M["channel"]["ratestatus"]);
-	setInterval(async() => {
-		if (channel){
-			let host = M["host"] + "/bot/ratecheck.php";
-			let key = await reqp ({
-				uri: host,
-				json: true
-				});
-			setTimeout(() => {
-				req.get(host + "?verify=" + key["key"], (err, res, body) => {
-					if (res.statusCode !==200) return;
-					if (body == "") return;
-					if (body.indexOf("<br")  !== -1) return;
-					let levels = JSON.parse(body);
-					if (levels["err"]) return;
-					if (levels["rated"].length == 0) return;
-					levels["rated"].forEach(level => {
-						let embed = new Discord.MessageEmbed();
-						embed.setTitle(`${level["users"]} rated ${level["name"]}`);
-						let info = level["text"] + "\n";
-						info += "==============\n";
-						info += `**LevelID**: ${level["ID"]}\n`;
-						embed.setDescription(info);
-						channel.send(embed);
-						});
-					}, 5000);
-				});
-			} else {
-				console.log("Error: channel is not found");
-				}
-			}, 10000);
-	}

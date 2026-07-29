f2bff1940782923545c58809aa0254c83d102ad9
tungdps <tunsena2k14@gmail.com>
2026-07-30 01:32:03 +0700

    Update leaderboard.js

--- cmds/leaderboard.js
+++ cmds/leaderboard.js
@@ -1,39 +1,39 @@
-const Discord = require ("discord.js");
-const fetch = require ("snekfetch");
-const M = require ("../setup.json");
+const { EmbedBuilder } = require("discord.js");
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const M = require("../setup.json");
 const http = M.host + "/bot/api/leaderboard.php";
 
 module.exports.run = async (client, msg, args) => {
-	
-	let page = args[1];
-	let type = args[0];
-	
-	if (!type) return msg.channel.send ("Try this command `" + M.prefix + "leaderboard` `stars` `1`");
-	if (page){
-		if (isNaN(page)) return msg.channel.send ("Please enter a number. not a letters");
-		}
-	//if (isNaN(!type)) return msg.channel.send ("Invalid ID");
-	
-	let fetchhost;
-	if (page){
-		fetchhost = http + "?in=" + type + "&page=" + page;
-		} else {
-			fetchhost = http + "?in=" + type;
-			}
-	
-	fetch.get (fetchhost).then( r => {
-    let body = r.body;
-	if (!body) return msg.channel.send ("Please use `stars` or `demon`");
-	if (!body.top) return msg.channel.send ("Page leaderboard `" + type + "` is not found");
-	
-	let embed = new Discord.MessageEmbed ()
-	.setTitle("Leaderboards of " + body.type )
-	.addField ("Top " + body.topTo , body.top)
-	.addField ( "__For Next Page__ ", "`" + M.prefix + "leaderboard " + type + " <page-num> `")
-	.setFooter ("Now Page = " + body.page)
-	msg.channel.send ({embed: embed});
-	});
-}
+    let page = args[1];
+    let type = args[0];
+    
+    if (!type) return msg.channel.send("Try this command `" + M.prefix + "leaderboard` `stars` `1``);
+    if (page && isNaN(page)) return msg.channel.send("Please enter a number, not letters");
+    
+    let fetchhost = page ? `${http}?in=${type}&page=${page}` : `${http}?in=${type}`;
+    
+    try {
+        let res = await fetch(fetchhost);
+        let body = await res.json();
+
+        if (!body) return msg.channel.send("Please use `stars` or `demon`");
+        if (!body.top) return msg.channel.send("Page leaderboard `" + type + "` is not found");
+        
+        let embed = new EmbedBuilder()
+            .setTitle("Leaderboards of " + body.type)
+            .addFields(
+                { name: "Top " + body.topTo, value: body.top },
+                { name: "__For Next Page__", value: "`" + M.prefix + "leaderboard " + type + " <page-num>`" }
+            )
+            .setFooter({ text: "Now Page = " + body.page });
+
+        return msg.channel.send({ embeds: [embed] });
+    } catch (err) {
+        console.error(err);
+        return msg.channel.send("Lỗi khi kết nối đến API Leaderboard!");
+    }
+};
+
 module.exports.help = {
-	name: "leaderboard"
-	}
+    name: "leaderboard"
+};

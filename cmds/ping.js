84f9bf6342cadaf6b7d88f6365f7ebc23d9b0401
tungdps <tunsena2k14@gmail.com>
2026-07-30 21:04:31 +0700

    Update ping.js

--- cmds/ping.js
+++ cmds/ping.js
@@ -1,9 +1,14 @@
-module.exports.run = async (client, msg, args) => {
-	let from = msg.createdTimestamp;
-	let m = await msg.channel.send ("pinging");
-	m.edit("Pong, my ping is " + `${"`"}${m.createdTimestamp - from}ms${"`"}`);
-	}
-	
-module.exports.help = {
-	name: "ping"
-	}
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    // Khai báo tên và mô tả lệnh Slash (Discord bắt buộc viết chữ thường, không khoảng trắng)
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Kiểm tra độ phản hồi của Bot TunGD'),
+
+    // Hàm thực thi khi người dùng gõ /ping
+    async execute(interaction) {
+        const ping = interaction.client.ws.ping;
+        await interaction.reply({ content: `🏓 Pong! Độ trễ hiện tại là **${ping}ms**` });
+    },
+};

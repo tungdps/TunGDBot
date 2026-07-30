87247f1c7528cf94273acf9c24751534f7718ef6
tungdps <tunsena2k14@gmail.com>
2026-07-30 21:42:01 +0700

    Update reboot.js

--- cmds/reboot.js
+++ cmds/reboot.js
@@ -1,18 +1,12 @@
-const M = require ("../setup.json");
+const { SlashCommandBuilder } = require('discord.js');
 
-module.exports.run = async (client, msg, args) => {
-	
-	let owo = msg.author.id == M.owner;
-	if (!owo) return;
-	msg.channel.send('See You Again ;)').then((m) => {
-        client.destroy().then(() => {
-          client.login(process.env.BOT_TOKEN).then (() => {
-          	m.edit('Hello, I am Back!');
-          });
-        });
-      });
-     }
-     
-module.exports.help = {
-	name: "boot"
-	}
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('reboot')
+        .setDescription('Restart the bot (Admin only)'),
+
+    async execute(interaction) {
+        await interaction.reply({ content: '🔄 Restarting TunGDBot...' });
+        process.exit(0);
+    }
+};

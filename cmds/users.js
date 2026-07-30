5f736709c24ba47936e660376f374ed123eaa1cf
tungdps <tunsena2k14@gmail.com>
2026-07-30 21:46:41 +0700

    Update users.js

--- cmds/users.js
+++ cmds/users.js
@@ -1,29 +1,17 @@
-const Discord = require ('discord.js');
-const fetch = require ('snekfetch');
-const M = require('../setup.json');
+const { SlashCommandBuilder } = require('discord.js');
 
-module.exports.run = async (client, msg, args) => {
-	let sms = await msg.channel.send ("Searching, Please wait");
-	
-	let query = msg.content.slice((M.prefix + "users").length + 1);
-		fetch.get(M.host + "/bot/api/accounts.php?ID=" + query).then( ac => {
-			let acc = ac.body;
-		sms.delete ();
-		if (!query) return msg.channel.send ("Give me the `AccountID` or `Playername`");
-		if (acc.msg) return msg.channel.send ("Sorry, Not Found");
-		
-		let embed = new Discord.MessageEmbed()
-		.setAuthor ("Stats of " + acc.user)
-		.setDescription (
-acc.stars + "\n" + acc.demon + "\n" + acc.S_Coins + "\n" + acc.CP )
-        .addField ("Social", acc.ytURL + "\n" + acc.twitter + "\n" + acc.twitch )
-        .addField ( "__Ban Stats__",  "Leaderboard Ban: " + acc.Ban + "\n" + "CP Ban: "  + acc.cpBanned, true)
-        .addField ("__Other Stats__" , "Admin Server: **" + acc.Admin + "**\nModerator: **" + acc.stat + "**" + acc.mod , true)
-		.setFooter ("AccountID: " + acc.id + " | Registered at: " + acc.register)
-		msg.channel.send({embed: embed});
-	});
-}
-		
-module.exports.help = {
-	name: "users"
-	}
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('users')
+        .setDescription('Search player info in GDPS')
+        .addStringOption(option => 
+            option.setName('username')
+                .setDescription('Account username')
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        const username = interaction.options.getString('username');
+        await interaction.reply({ content: `👤 Searching user info for: **${username}**` });
+    }
+};
